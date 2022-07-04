4dbedd393ec8b65f19fddca9695e0d15aabc4a51
Eleomar Pedro Lorenzo <eleomarpedrolorenzo@gmail.com>
2022-07-04 09:04:05 -0500

    feat: Base config in App and main jsx with fontsource roboto

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { ToastContainer } from 'react-toastify'
+
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'react-toastify/dist/ReactToastify.css'
+import '@fontsource/roboto'
+
 const App = () => {
   return (
-    <p>Hola mundo con Vite</p>
+    <>
+      <p>Hola mundo con Vite</p>
+      <ToastContainer position="top-right"
+        autoClose={ 5000 } hideProgressBar={ false }
+        newestOnTop={ false } closeOnClick
+        rtl={ false } pauseOnFocusLoss
+        draggable={ false } pauseOnHover
+      />
+    </>
   )
 }
-
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 
 import App from './App'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
-  <App />
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 )
