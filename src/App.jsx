8c7c3d940759835829f8e9e1ea8d18d518d22547
Eleomar Pedro Lorenzo <eleomarpedrolorenzo@gmail.com>
2022-07-04 08:49:49 -0500

    initialized project

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+const App = () => {
+  return (
+    <p>Hola mundo con Vite</p>
+  )
+}
+
+export default App
---
+++ src_electron/main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  const createMainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      nativeWindowOpen: true
+    }
+  })
+
+  // createMainWindow.loadFile('index.html')
+  createMainWindow.maximize()
+  createMainWindow.loadURL('http://localhost:3000')
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
