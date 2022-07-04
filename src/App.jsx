95dab9a9710e991303b0b965f4022b880c5db7cc
Eleomar Pedro Lorenzo <eleomarpedrolorenzo@gmail.com>
2022-07-04 18:50:47 -0500

    fix: Admin and User routes was changed

--- src/App.jsx
+++ src/App.jsx
@@ -33,8 +33,8 @@ const App = () => {
             }
           />
         </Route>
-        <Route path="admin" element={ <AdminRoute /> }>
-          <Route index
+        <Route element={ <AdminRoute /> }>
+          <Route path="admin/*"
             element={
               <>
                 <Suspense fallback={ <SpinnerLoadingPage /> }>
@@ -44,8 +44,8 @@ const App = () => {
             }
           />
         </Route>
-        <Route path="user" element={ <UserRoute /> }>
-          <Route index
+        <Route element={ <UserRoute /> }>
+          <Route path="user/*"
             element={
               <>
                 <Suspense fallback={ <SpinnerLoadingPage /> }>
