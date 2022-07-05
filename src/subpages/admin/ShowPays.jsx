4bc83d8328e983a4a03410079b258ade594cda2e
Eleomar Pedro Lorenzo <eleomarpedrolorenzo@gmail.com>
2022-07-05 01:12:06 -0500

    feat: Show pays page was added without functionality

--- src/subpages/admin/ShowPays.jsx
+++ src/subpages/admin/ShowPays.jsx
@@ -1,6 +1,28 @@
+import { useState } from 'react'
+
+import PersonalizedTable from '../../components/common/PersonalizedTable'
+import TitlePage from '../../components/common/TitlePage'
+import Searcher from '../../components/views/Searcher'
+
 const ShowPays = () => {
+  const [searcher, setSearcher] = useState('')
+  const [listPays, setListPays] = useState([])
+
   return (
-    <p>Ver Pagos</p>
+    <>
+      <TitlePage>Lista de pagos exitosos</TitlePage>
+      <Searcher setSearcher={ setSearcher } />
+      <PersonalizedTable>
+        <thead>
+          <tr>
+            <th>Articulo</th>
+            <th>Descripcion</th>
+            <th>Deudor</th>
+            <th>Total</th>
+          </tr>
+        </thead>
+      </PersonalizedTable>
+    </>
   )
 }
 
