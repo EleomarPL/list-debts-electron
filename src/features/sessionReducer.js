93e32c13ad8080aa2c3673f0f7f35ce0c9fb81d3
Eleomar Pedro Lorenzo <eleomarpedrolorenzo@gmail.com>
2022-07-05 19:26:34 -0500

    fix: Recovery data of local storage and login action was added

--- src/features/sessionReducer.js
+++ src/features/sessionReducer.js
@@ -3,16 +3,19 @@ import { createSlice } from '@reduxjs/toolkit'
 export const sessionSlice = createSlice({
   name: 'session',
   initialState: {
-    value: {},
+    value: JSON.parse(window.localStorage.getItem('user')) || {},
     status: 'idle'
   },
   reducers: {
     logout: state => {
       state.value = {}
+    },
+    login: (state, action) => {
+      state.value = action.payload
     }
   }
 })
 
-export const { logout } = sessionSlice.actions
+export const { logout, login } = sessionSlice.actions
 
 export default sessionSlice.reducer
