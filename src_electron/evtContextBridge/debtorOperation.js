f983bd6a1ee01d2094e965d4910936e8c2d3f26e
Eleomar Pedro Lorenzo <eleomarpedrolorenzo@gmail.com>
2022-07-08 11:54:01 -0500

    fix: Repair operations of debts in main proccess

--- src_electron/evtContextBridge/debtorOperation.js
+++ src_electron/evtContextBridge/debtorOperation.js
@@ -1,29 +1,29 @@
 const { ipcMain } = require('electron')
 
 const { insertDebtor, getDebtors, updateDebtor } = require('../db/debtor')
-const { filterAdmin } = require('../middlewares/stractortoken')
+const { stractorToken } = require('../middlewares/stractortoken')
 
 const triggerEventsDebtor = () => {
   ipcMain.handle('debtor:getDebtors', async (_, { token }) => {
-    const idUser = filterAdmin({ token })
+    const idUser = stractorToken({ token })
     if (!idUser) return false
 
     return await getDebtors()
   })
   ipcMain.handle('debtor:insertDebtor', async (_, { token, name, lastName, motherLastName, address }) => {
-    const idUser = filterAdmin({ token })
+    const idUser = stractorToken({ token })
     if (!idUser) return false
 
     return await insertDebtor({
-      idUser, name, lastName, motherLastName, address
+      idUser: idUser.id, name, lastName, motherLastName, address
     })
   })
   ipcMain.handle('debtor:updateDebtor', async (_, { token, idDebtor, address }) => {
-    const idUser = filterAdmin({ token })
+    const idUser = stractorToken({ token })
     if (!idUser) return false
 
     return await updateDebtor({
-      address, idUser, idDebtor
+      address, idUser: idUser.id, idDebtor
     })
   })
 }
