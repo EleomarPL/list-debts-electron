773d1b111100a3c236667b5b3e78258cbb6a4b14
Eleomar Pedro Lorenzo <eleomarpedrolorenzo@gmail.com>
2022-08-30 11:46:51 -0500

    feat: Borders to table was added

--- src/components/common/PersonalizedTable.jsx
+++ src/components/common/PersonalizedTable.jsx
@@ -3,7 +3,10 @@ import styled from 'styled-components'
 const PersonalizedTable = styled.table`
   width: 100%;
   text-align: center;
-  color: var(--color-primary);
+  color: var(--text-primary);
+  & tr{ border: 1px solid var(--text-primary); }
+  & td{ padding: 10px 0 !important; }
+  & th{ border: 1px solid var(--text-primary); }
 `
 
 export default PersonalizedTable
